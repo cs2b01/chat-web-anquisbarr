9fd77d7b7b9a7f935beb690a5e6ca31716ab5b1a
Sebastian Q.B <sebastian.quispe.b@utec.edu.pe>
2019-05-29 11:07:23 -0500

    Subiendo primer avance

--- web/server.py
+++ web/server.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/static/<content>')
 def static_content(content):
     return render_template(content)
@@ -38,13 +40,56 @@ def get_user(id):
     message = { 'status': 404, 'message': 'Not Found'}
     return Response(message, status=404, mimetype='application/json')
 
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
+@app.route('/users', methods = ['POST'])
+def create_user():
+    c =  json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    return 'Created User'
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(user, key, c[key])
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+
+
+@app.route('/messages', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    messages = session.query(entities.User).filter(entities.User.id == id)
+    for message in messages:
+        session.delete(message)
+    session.commit()
+    return "Deleted Message"
+
+@app.route('/users', methods = ['DELETE'])
+def delete_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        delattr(user, key, c[key])
+    session.delete(user)
+    session.commit()
+    return 'Deleted User'
+
 
 if __name__ == '__main__':
     app.secret_key = ".."
